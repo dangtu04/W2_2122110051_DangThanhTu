6beff2a1e4ab24aa1556a13ec7bf35471c395507
DangThanhTu <dangtu22304@gmail.com>
2025-03-03 07:40:44 +0700

    03-03-2025

---
+++ src/main/java/com/example/demo/DangthanhtuApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangthanhtuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangthanhtuApplication.class, args);
+	}
+
+}
